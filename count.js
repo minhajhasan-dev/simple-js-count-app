17b36a18a1f3d837ec1543f56d09350346adb0da
minhaj.dev <minhajhasan.dev@gmail.com>
2024-02-17 21:54:15 +0600

    added validation for negative count

--- count.js
+++ count.js
@@ -1,5 +1,6 @@
 let sum = 0;
 const countElement = document.getElementById("count");
+
 function increment() {
   sum++;
   countElement.innerText = sum;
@@ -7,6 +8,10 @@ function increment() {
 
 const decrementBtn = document.getElementById("decrement");
 decrementBtn.addEventListener("click", function () {
-  sum--;
-  countElement.innerText = sum;
+  if (sum > 0) {
+    sum--;
+    countElement.innerText = sum;
+  } else {
+    alert("Count can't be less than 0.");
+  }
 });
